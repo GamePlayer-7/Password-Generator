9b005d8c0a479d482574635a2909404bdc37501b
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2021-06-03 11:45:12 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+print('Welcome to Password generator!')
+
+numalpha=int(input('\nNumber of alphabets: '))
+numdigits=int(input('Number of Digits: '))
+numspchar=int(input('Number of Special characters: '))
+
