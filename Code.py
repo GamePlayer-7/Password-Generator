6b62aa66d8d3b73d4258909bb2cc93a7876683ba
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2021-06-04 11:42:05 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,3 +7,5 @@ numalpha=int(input('\nNumber of alphabets: '))
 numdigits=int(input('Number of Digits: '))
 numspchar=int(input('Number of Special characters: '))
 
+if numalpha+numdigits+numspchar >= 8:
+  tempalpha=tempdigits=tempspchar=''
