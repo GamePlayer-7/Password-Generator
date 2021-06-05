56e2b7e8a23f82ea3874e28fb4e083a9328a2559
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2021-06-05 13:41:57 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -9,3 +9,10 @@ numspchar=int(input('Number of Special characters: '))
 
 if numalpha+numdigits+numspchar >= 8:
   tempalpha=tempdigits=tempspchar=''
+  
+ for i in range(numalpha):
+    tempalpha=tempalpha+(random.choice(string.ascii_letters))
+  for i in range(numdigits):
+    tempdigits=tempdigits+(random.choice(string.digits))
+  for i in range(numspchar):
+    tempspchar=tempspchar+(random.choice('!@$%^&*?'))
